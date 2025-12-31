656cd117f5d03270309d9e201ff842178251b67f
Kiran Sodheja <130165244+Kiran-Sodheja@users.noreply.github.com>
2025-12-31 15:26:34 +0530

    Update Article views.sql

--- Easy/Article views.sql
+++ Easy/Article views.sql
@@ -1,1 +1,7 @@
 select distinct author_id as id from Views where author_id=viewer_id order by id asc
+
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE viewer_id >= 1
+AND author_id = viewer_id
+ORDER BY author_id
