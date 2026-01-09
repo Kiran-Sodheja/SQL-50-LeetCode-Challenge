89aad39381dd5929873469c8236429fea51e6ad6
Kiran Sodheja <130165244+Kiran-Sodheja@users.noreply.github.com>
2026-01-09 18:18:45 +0530

    Create Percentage of Users Attended a Contest.sql

---
+++ Easy/Percentage of Users Attended a Contest.sql
@@ -0,0 +1,1 @@
+select r.contest_id,round(count(distinct r.user_id)*100/(select count(*)from users),2) as percentage from Register as r group by r.contest_id order by percentage desc,r.contest_id asc;
